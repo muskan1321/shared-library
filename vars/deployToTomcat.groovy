596984a09509f4059e2062d17fc5a23e13682040
muskan1321 <130132132+muskan1321@users.noreply.github.com>
2023-10-18 13:01:00 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -1,19 +1,11 @@
-def call(credentialId, userName, serverIp, warfileName){
-   sshagent([credentialId]) {
-   for (ip in serverIps) {
-    sshagent([credentialId]) {
-        sh "scp -o StrictHostKeyChecking=no target/${warfileName} ${userName}@${ip}:/opt/tomcat9/webapps"
+def call(credentialId, userName, ip, warfileName){
+      sshagent([credentialId]) {
+        sh "scp -o StrictHostKeyChecking=no target/${warfileName}.war ${userName}@${ip}:/opt/tomcat9/webapps"
         sh "ssh ${userName}@${ip} /opt/tomcat9/bin/shutdown.sh"
         sh "ssh ${userName}@${ip} /opt/tomcat9/bin/startup.sh"
       }
-  }
+  
 }
-
-// Example usage of the call method
-def serverIps = ['172.31.19.102']
-def warFileName = 'doctor-online.war'
-call("tomcat-dev", "ec2-user", serverIps, warFileName)
-   
 
 
 
